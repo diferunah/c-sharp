7b3592bd3b4ad374fb2a697f179c87e0c9e5efce
Dixee Molina <dfmolina@unah.hn>
2020-10-26 01:51:08 -0600

    modificando propiedades

--- EJERCICIOS/05 - propiedades/propiedades/Alumnos.cs
+++ EJERCICIOS/05 - propiedades/propiedades/Alumnos.cs
@@ -5,4 +5,16 @@ public class Alumno
     public string PrimerNombre { get; set; }
 
     public string SegundoNombre { get; set; }
+
+    public Alumno() //constructores
+    {
+        Id = 1;
+    }
+
+    public Alumno(int Id) //constructores con declaracion 
+    {
+        Id = Id;
+    }
+
 }
+
--- EJERCICIOS/05 - propiedades/propiedades/Program.cs
+++ EJERCICIOS/05 - propiedades/propiedades/Program.cs
@@ -6,12 +6,21 @@ namespace propiedades
     {
         static void Main(string[] args)
         {
-            Alumno a = new Alumno();
-            a.Id = 1;
+            Alumno a = new Alumno(); //llama al id del constructor
             a.PrimerNombre = "Juan";
             a.SegundoNombre = "Perez";
 
-            Console.WriteLine("Hello World!");
+            Alumno b = new Alumno(); //llama al id del constructor
+            b.Id = 2;               // sobreescrbe el valor de constructor
+            b.PrimerNombre = "Maria";
+            b.SegundoNombre = "Martinez";
+
+            Alumno c = new Alumno(3); //llama al 2do constructor con variable declarada
+            b.PrimerNombre = "Pedro";
+
+            Console.WriteLine(a.Id);
+            Console.WriteLine(b.Id);
+            Console.WriteLine(c.Id);
         }
     }
 }
